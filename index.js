160154c64f09fbd411f5e766bf7373abf7bf340e
Alfonso D Rojas Hernandez <acostade@gmail.com>
2020-07-30 19:52:21 +0000

    FizzBuzz Question Start

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-hhhdddd
+//1 Fizzbuzz Question//
+
+// Prin the numbers from 1 to 100 and for multiples of '3' print "Fizz" instead of the number and for the multiples of '5' print "Buz"//
+
+// Output: 1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,//
+// Buzz, 11,Fizz, 13, 14, Fizz buzz,16 ,17 ...
+
